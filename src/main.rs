19dc93d12d17c07d360e1fa1042c69b3b3619600
emmanuelist <emmanuel.paul75@yahoo.com>
2024-11-04 19:47:22 +0100

    Refactor main function to read user input for array index and display corresponding element

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+use std::io;
+
 fn main() {
-    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let a = [1, 2, 3, 4, 5];
 
-    let five_hundred = x.0;
-    println!("The value of five_hundred is: {}", five_hundred);
+    println!("Please enter an array index.");
 
-    let six_point_four = x.1;
-    println!("The value of six_point_four is: {}", six_point_four);
+    let mut index = String::new();
 
-    let one = x.2;
-    println!("The value of one is: {}", one);
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
